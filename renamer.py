b1ac993a6b6e0110bc7b46f49174bec8d57ceaac
Prajan Pradhan <prajan.prdn@gmail.com>
2019-08-28 11:22:48 -0500

    Renamer built in python added

---
+++ renamer.py
@@ -0,0 +1,7 @@
+import os
+os.chdir('/home/prajan/Projects/AutoParsing/folder')
+for f in os.listdir():
+        f_name, f_num = f.split('-')
+        new_name='{}-{}'.format(f_num,f_name)
+        os.rename(f, new_name)
+
